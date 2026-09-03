843e69abe01bf2571cac050c06e905c05e343356
Stanley-baby <58399659+Stanley-baby@users.noreply.github.com>
2026-09-03 14:10:56 +0800

    Preserve bookmark scroll position after removals (#19)

--- src/co/bookmarks/container/scroll.js
+++ src/co/bookmarks/container/scroll.js
@@ -1,12 +1,36 @@
 import s from './scroll.module.styl'
 import _ from 'lodash-es'
-import React, { useRef, useCallback } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useRef, useCallback, useLayoutEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { nextPage } from '~data/actions/bookmarks'
+import { bookmarksIds, query as getQuery } from '~data/selectors/bookmarks'
+import { queryIsEqual } from '~data/helpers/bookmarks'
 
 export default function BookmarksContainerScroll({ spaceId, children }) {
     const div = useRef(null)
     const dispatch = useDispatch()
+    const ids = useSelector(state=>bookmarksIds(state, spaceId))
+    const query = useSelector(state=>getQuery(state, spaceId))
+    const previous = useRef({ ids, query })
+    const scrollTop = useRef(0)
+
+    useLayoutEffect(()=>{
+        if (!div.current) return
+
+        //Removing a bookmark can make virtualized children reflow and reset
+        //the scroll container. Restore the position captured by onScroll.
+        const wasRemoved = (
+            previous.current.ids.length > ids.length &&
+            queryIsEqual(previous.current.query, query) &&
+            ids.every(_id=>previous.current.ids.includes(_id))
+        )
+
+        if (wasRemoved)
+            div.current.scrollTop = scrollTop.current
+
+        previous.current = { ids, query }
+        scrollTop.current = div.current.scrollTop
+    }, [ids, query])
 
     const loadNextPage = useCallback(
         _.throttle(()=>{
@@ -18,11 +42,18 @@ export default function BookmarksContainerScroll({ spaceId, children }) {
         [div, spaceId]
     )
 
+    const onScroll = useCallback(()=>{
+        if (div.current)
+            scrollTop.current = div.current.scrollTop
+
+        loadNextPage()
+    }, [loadNextPage])
+
     return (
         <div 
             ref={div}
             className={s.scroll}
-            onScroll={loadNextPage}>
+            onScroll={onScroll}>
             {children}
         </div>
     )
